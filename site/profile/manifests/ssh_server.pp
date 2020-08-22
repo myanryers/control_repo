d8212912f63d790f9e84186ac2034c5fc34c998a
myanryers <38222111+myanryers@users.noreply.github.com>
2020-08-21 21:33:37 -0500

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,15 @@
+class profile::ssh_server {
+  package {'openssh-server':
+    ensure => present,
+  }
+  service {'sshd':
+    ensure => 'running',
+    enable => 'true',
+  }
+  ssh_authorized_keys { 'root@master.puppet.vm':
+    ensure => present,
+    user => 'root',
+    type => 'ssh-rsa',
+    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDseLPvlNtj0Lmd12OlX8L/Iy5reRyk2OQAOlHBhONuFnoiP+i2ab/tdtl38Z34toYTSktjW65cwHwp2Sb43EOWIk5wKtqgE8D5nNzfZMSWCXM8oJdsv89a2KClLePdM467Kjf7bjzRb4qIvlTcpWroFBDu37+UslTHV3ipA64ELmTUin5FC/wwkZOMtsC8OqkvBCV+Vq5rGAF/e8oEZwSQ+1gQjaSiNdjsCxxrtiFNqY41CMzRFiWBxiXL1fnko/kONmGfeA4eBhauzGReKZUllRNKbyBvwVLVfmEyfPcckgyRd2MmTKzrkkcCxwMg3avgz5JBFhgx0wXWaMaY7nBX',
+  }
+}
