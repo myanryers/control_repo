e722a5055092b5fa5b633ce598ebba20ba9d84fc
myanryers <38222111+myanryers@users.noreply.github.com>
2020-08-21 22:43:42 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft {
   file {'/opt/minecraft':
     ensure => directory,
   }
-  file {'/opt/mincraft/server.jar':
+  file {'/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/c5f6fb23c3876461d46ec380421e42b289789530/server.jar',
   }
